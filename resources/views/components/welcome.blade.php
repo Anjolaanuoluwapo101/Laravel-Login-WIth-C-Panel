32dc4d187c0f2bda94e3520834c9777dcb72ce9d
Anjolaanuoluwapo <anjolaakinsoyinu@gmail.com>
2025-04-24 06:33:39 +0100

    deployed to Laravel clouds,no issue.Just a button fixed

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -3,12 +3,10 @@
         Welcome {{ auth()->user()->name }} to your dashboard!
     </h1>
 
-    <button
-        type="button"
+    <a href="{{ route('services.panel') }}"
         class="mt-6 inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-        wire:click="$emit('openPasswordConfirmModal')"
     >
         Go to Services Panel
-    </button>
+    </a>
 
 </div>
