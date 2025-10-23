29858da102f33240635e85c0da4bb5795ef886b8
Akinsoyinu Anjola Anuoluwapo <111210173+Anjolaanuoluwapo101@users.noreply.github.com>
2025-10-23 12:37:47 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        URL::forceScheme('https');
+        if (env('APP_ENV') == 'production') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
